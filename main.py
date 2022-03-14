6e6d5b544aa13c5c3d3995faa482c52399c702f9
joaop5142 <joaopaulo3687@hotmail.com>
2022-03-13 22:26:30 -0300

    Refactoring functions and logic

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def print_report():
     print(f"Coffee: {coffe}g")
     print(f"Money:${format_money}")
 
-def verify_resources(coffee,coins_value):
+def verify_resources(coffee):
     """Verify and return if resources are sufficient"""
     drink=MENU[coffee]
     drink_cost=drink["cost"]
@@ -55,23 +55,25 @@ def verify_resources(coffee,coins_value):
         drink_milk=0
     drink_coffee=drink["ingredients"]["coffee"]
 
-    can_buy=False
-    if(coins_value>=drink_cost):
-        if(resources["water"]<drink_water):
-             print( "Sorry there if not enough water.")
-        elif(resources["milk"]<drink_milk):
-            print("Sorry there if not enough milk.")
-        elif (resources["coffee"]<drink_coffee):
-            print("Sorry there if not enough coffee.")
-        else:
-            print(f"Here is your {answer}☕.Enjoy!")
-            can_buy=True
-    else:
-        print("Sorry there is not enough money.")
+    can_buy=True
+
+    if(resources["water"]<drink_water):
+        print( "Sorry there is not enough water.")
+        return False
+    elif(resources["milk"]<drink_milk):
+        print("Sorry there is not enough milk.")
+        return False
+    elif (resources["coffee"]<drink_coffee):
+        print("Sorry there is not enough coffee.")
+        return False
+
+
 
     return can_buy
 
 def process_coins():
+    print("Please insert coins.")
+
     quarters_value=int(input("how many quarters?: "))
     dimes_value=int(input("how many dimes?: "))
     nickles_value=int(input("how many nickles?: "))
@@ -91,6 +93,7 @@ def process_payment(coffee,coins_value):
         print(f"Here is ${format_money} dollars in change.")
         return drink_value
     else:
+        print(f"")
         return drink_value
 
 def build_coffe(coffee):
@@ -106,27 +109,37 @@ def build_coffe(coffee):
     resources["water"]=resources["water"]-drink_water
     resources["coffee"] = resources["coffee"] - drink_coffee
 
+def transaction_successful(money,drink_cost):
+    """Return true when the payment is accepted,or False if moneys is insufficient"""
+    if(money>=drink_cost):
+        return True
+    else:
+        print("Sorry that's not enough money.Money refunded.")
+
 # TODO 1. Ask to the user input
 # TODO 2. Verify if the resources are sufficient
 # TODO 3. Insert coins
 # TODO 4. Verify if coins is enoghout to buy
 
 while should_continue:
-    answer=input("What Would you like ? (espresso/latte/cappuccino): ").lower()
-    print("Please insert coins.")
-
-    if(answer=="off"):
+    choice=input("What Would you like ? (espresso/latte/cappuccino): ").lower()
+    if(choice== "off"):
         should_continue=False
         break
-    elif(answer=="report"):
+    elif(choice == "report"):
         print_report()
-    elif(answer== "latte")or (answer=="espresso")or(answer=="cappuccino"):
-       coins_value=process_coins()
-       can_buy= verify_resources(answer,coins_value)
+    elif(choice == "latte")or (choice == "espresso")or(choice == "cappuccino"):
+       resources_sufficient = verify_resources(choice)
+       if(resources_sufficient):
+            coins_value=process_coins()
+            drink_cost=MENU[choice]["cost"]
+            if(transaction_successful(coins_value,drink_cost)):
+                can_buy=True
+
 
     if(can_buy):
-       cost+= process_payment(answer,coins_value)
-       build_coffe(answer)
+       cost+= process_payment(choice, coins_value)
+       build_coffe(choice)
        can_buy = False
 
 
